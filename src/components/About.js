da3500592d56b6539b290bf0fd5dad05b8d5eb5b
aquibk580 <148651904+aquibk580@users.noreply.github.com>
2024-03-10 15:39:59 +0530

    About.js Updated

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,16 @@ const About = () => {
   return (
     <>
       <h1>
-        This is About page
+       This App is created by <br /> Mr.Aquib Khan <br />
+        Technologies used in this app: <br />
+        1.HTML <br />
+        2.CSS <br />
+        3.JavaScript <br />
+        4.Bootstrap <br />
+        5.Mongo DB <br />
+        7.Express js <br />
+        6.React js <br />
+        7.Node js 
       </h1>
     </>
   );
